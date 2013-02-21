61fbff4a398e72c68d8651676f2e96e41553b5e4
Dosantiago <douglas_sa_90@hotmail.com>
2013-02-20 23:56:13 -0300

    conexão

---
+++ SistemaNatacao/src/br/edu/utfpr/cm/tsi/projetointegrador/gui/Produto/Conexao.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.gui.Produto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ana
+ */
+public class Conexao {
+
+    /**  Variavel de conexao */
+    private Connection con = null;
+    /**  Variavel do Driver */
+    private String driver;
+    /**  Variavel que guarda o valor do usuario */
+    private String usuario;
+    /**  Variavel que guarda o valor da senha */
+    private String senha;
+    /**  Variavel que guarda o valor da string de conexao */
+    private String endereco;
+
+    /**
+     * Construtor default
+     */
+    public Conexao() {
+        driver = "com.mysql.jdbc.Driver";
+        usuario = "root";
+        senha = "douglas";
+        endereco = "jdbc:mysql://localhost:3306/sistemanatacao";
+    }
+
+    /**
+     * Cria e retorna a conexao do driver JDBC ao banco de dados
+     */
+    public Connection getConnection() {
+        try {
+            Class.forName(this.driver);
+            con = DriverManager.getConnection(endereco, usuario, senha);
+        } catch (ClassNotFoundException erro) {
+            System.out.println("Falha ao carregar o driver JDBC/ODBC." + erro);
+            return null;
+        } catch (SQLException erro) {
+            System.out.println("Falha na conexao, comando sql = " + erro);
+            return null;
+        } catch (Exception erro) {
+            erro.getMessage();
+            System.out.println("Nao foi possivel efetuar a conexao!");
+            return null;
+        }
+        return con;
+    }
+}
+
+   
